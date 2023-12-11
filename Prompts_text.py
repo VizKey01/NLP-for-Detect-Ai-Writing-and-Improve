9a91bf984a9228e93bd52a80863380679ee0dc0c
VizKey01 <micmin0101@gmail.com>
2023-12-12 00:01:38 +0700

    change code

--- Prompts_text.py
+++ Prompts_text.py
@@ -9,21 +9,39 @@ prompt1 = """Act as an AI writing tutor in English. You will receive a
             Don't say anything at first. Wait for the user to say something.
         """
 
-prompt2 = """Act as an AI writing analyzer in English. You will receive a piece of writing and your task is to:
+prompt2 = """Act as an AI writing analizer, and rewrite the text.
+            I need two types of output:
+            1. Rewrite the text to make it more human-like with increased perplexity and burstiness. And present it as a string.
+            2. Identify Suggestions from the rewrote text. Only share the text generated from the rewriting, listing each suggestion in a JSON array format with the fields:
+           
+            'before': The text before the suggested change.
+            'after': The text after implementing the suggestion.
+            'category': The category of the suggestion (e.g., 'grammar', 'style', 'word choice', 'other').
+            'comment': A comment or explanation about the suggested change.
 
-            1. Rewrite the text to make it more human-like with increased perplexity and burstiness.
-
-            2. Provide suggestions on how to enhance the text, listing each suggestion in a JSON array format with the fields:
-            - 'before': The text before the suggested change.
-            - 'after': The text after implementing the suggestion.
-            - 'category': The category of the suggestion (e.g., 'grammar', 'style', 'word choice', 'other').
-            - 'comment': A comment or explanation about the suggested change.
-
-            Format the output as follows:
-            [ "Text after change", [ { "before": "Text before change", "after": "Text after change", "category": "Category", "comment": "Comment about the change." } ] ]
-
+            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after 'Translated text string' to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
+            
+            [ 
+                "Rewrite text string", 
+                [ 
+                    { 
+                        "before": "Text before change1", 
+                        "after": "Text after change1", 
+                        "category": "Category1", 
+                        "comment": "Comment about the change1" 
+                    },
+                    { 
+                        "before": "Text before change2", 
+                        "after": "Text after change2", 
+                        "category": "Category2", 
+                        "comment": "Comment about the change2" 
+                    }
+                ] 
+            ]
+            This line is to remind you that THE MOST IMPORTANT thing is that the output should be in the same format as the example above. If not regenerate the output until it is in the same format as the example above.
             Don't say anything at first. Wait for the user to say something.
             """
+
 prompt3 = """Act as an AI writing translater, and translate text to {}.
             I need two types of output:
             1. Translate the entire text and present it as a string.
@@ -35,7 +53,7 @@ prompt3 = """Act as an AI writing translater, and translate text to {}.
             "Example" : an example sentence of the vocabulary in the language you just translate to.
             Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after 'Translated text string' to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
             [
-                "Text after change",
+                "Translated text string",
                 [
                     {{
                         "Vocabulary": "Vocabulary1",
@@ -49,7 +67,7 @@ prompt3 = """Act as an AI writing translater, and translate text to {}.
                         "Translation": "Translation2",
                         "Example": "Example2"
                     }}
-                    // ... for 5 vocabularies
+                    // ... for 10 vocabularies
                 ]
             ]
             This line is to remind you that THE MOST IMPORTANT thing is that the output should be in the same format as the example above. If not regenerate the output until it is in the same format as the example above.
@@ -91,15 +109,16 @@ prompt4 = """Act as an AI writing corrector, and correct the text tht you reciev
             Remember, THE MOST IMPORTANT thing is that the output should be in the same format as the example above. If not regenerate the output until it is in the same format as the example above.
             Don't say anything at first. Wait for the user to say something.
         """
+
 prompt5 = """Act as an AI writing summarize, and summarize the text.
             I need two types of output:
             1. Summarize the entire text and present it as a string.
             2. you must list the Identify Key points .List the Key points in a JSON array, one word per line. Only share the text generated from the summary, listing each key point in a JSON array format with the fields:
-            and put all together in a list.
+           
             "KeyPoint" : the key point identified in the original text
             "Context" : a sentence or paragraph from the original text that illustrates the key point
 
-            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after 'Translated text string' to separating the translation output and the vocabulary details before being returned as a response, lastly make sure that 'Translate text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
+            Format the output only as SAME as follows. ensuring that the translation output and the vocabulary details are properly separated into their respective list formats (string and dictionaries/JSON array) and don't forget to add a comma after 'Summarized text string' to separating the summary output and the keypoint details before being returned as a response, lastly make sure that 'Summarize text string' isn't a list but it's a string that in the same list as JSON array and the whole output is in list of string and JSON array format.:
             [ 
                 "Text after change",    
                 [
